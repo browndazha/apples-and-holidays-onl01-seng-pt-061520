f8490c9d430c5634c31470549c95459fb3028ed9
Dazha Brown <dajahbrown@hotmail.com>
2020-06-30 00:15:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -11,7 +11,7 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  holiday_supplies[:spring][:memorial_day] << "Grill"
+  holiday_supplies[:spring][:memorial_day] << "BBQ"
   holiday_supplies[:spring][:memorial_day] << "Table Cloth"
 end
 
