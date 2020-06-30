b974309568af7c66e26ef0a20225c3589e51f600
Dazha Brown <dajahbrown@hotmail.com>
2020-06-30 00:18:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -16,8 +16,6 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_supplies[:fall][:thanksgiving] = << "Turkey"
-  holiday_supplies[:winter][:valentines_day] = ["Cupid Cut-Out", "Candy Hearts"]
-  holiday_supplies
 end
 
 def all_winter_holiday_supplies(holiday_hash)
